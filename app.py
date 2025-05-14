36b20c6ba84b4ef1acc41cadd885860a541cf040
Siddharth Salve <siddharthsalve2000@gmail.com>
2025-05-14 23:11:14 +0530

    Fix: bind to 0.0.0.0 and use dynamic port

--- app.py
+++ app.py
@@ -33,4 +33,9 @@ def predict():
     return render_template('index.html', prediction_text=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port)
+
